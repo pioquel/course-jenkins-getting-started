d8b86db1ad983d71b5e48bfbe3ace90a745fef2b
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-06-07 10:11:45 -0400

    refactor back to the actual build process with notifications on changed build status only

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -10,17 +10,17 @@ pipeline {
         }
         stage('Build') {
             steps {
-                // sh './mvnw clean package'
-                sh 'false'
+                sh './mvnw clean package'
+                // sh 'false'
             }
 
             post {
                 always {
-                    // junit '**/target/surefire-reports/TEST-*.xml'
-                    // archiveArtifacts 'target/*.jar'
-                //}
+                    junit '**/target/surefire-reports/TEST-*.xml'
+                    archiveArtifacts 'target/*.jar'
+                }
 
-                //changed {
+                changed {
                     emailext subject: "Job \'${JOB_NAME}\' (buil ${BUILD_NUMBER}) ${currentBuild.result}",
                         body: "Please go to ${BUILD_URL} and verify the build",
                         compressLog: true,
