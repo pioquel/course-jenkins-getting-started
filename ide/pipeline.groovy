06b9a0d0542365691dea2779f4369b259c5161b3
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-06-07 10:01:08 -0400

    initial email that is sent on every build

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -10,13 +10,23 @@ pipeline {
         }
         stage('Build') {
             steps {
-                sh './mvnw clean package'
+                // sh './mvnw clean package'
+                sh 'false'
             }
 
             post {
                 always {
-                    junit '**/target/surefire-reports/TEST-*.xml'
-                    archiveArtifacts 'target/*.jar'
+                    // junit '**/target/surefire-reports/TEST-*.xml'
+                    // archiveArtifacts 'target/*.jar'
+                //}
+
+                //changed {
+                    emailext subject: "Job \'${JOB_NAME}\' (buil ${BUILD_NUMBER}) ${currentBuild.result}",
+                        body: "Please go to ${BUILD_URL} and verify the build",
+                        compressLog: true,
+                        attachLog: true, 
+                        to: "test@jenkings",
+                        recipientProviders: [upstreamDevelopers(), requestor()]
                 }
             }
         }
